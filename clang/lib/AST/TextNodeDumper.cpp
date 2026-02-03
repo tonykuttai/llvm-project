9c2b2fe99ea56670e01dfce24c25ddbbfd0392c9
Tony Varghese <tony.varghese@ibm.com>
2026-02-02 23:19:10 -0500

    [PowerPC][AIX] Support #pragma comment copyright for AIX
    - Emit !aix.copyright.comment from Clang for the pragma.
    - Lower it in LLVM to a TU-local string + llvm.used + !implicit.ref.
    - Add module-import and backend relocation tests.

--- clang/lib/AST/TextNodeDumper.cpp
+++ clang/lib/AST/TextNodeDumper.cpp
@@ -2602,6 +2602,9 @@ void TextNodeDumper::VisitPragmaCommentDecl(const PragmaCommentDecl *D) {
   case PCK_User:
     OS << "user";
     break;
+  case PCK_Copyright:
+    OS << "copyright";
+    break;
   }
   StringRef Arg = D->getArg();
   if (!Arg.empty())
